05d8dab63f26a4917d80ab3bb7f63df3851d943b
Snorre Ledal <snorreledal@hotmail.no>
2023-01-15 17:12:14 +0100

    exercise 1-12

--- src/exercise/1-12.c
+++ src/exercise/1-12.c
@@ -7,30 +7,13 @@
 
 /* count lines, words, and characters in input */
 main() {
-    int c, nl, nw, nc, state;
+    int c, state;
 
     state = OUT;
-    nl = nw = nc = 0;
     while ((c = getchar()) != EOF) {
-        ++nc;
-
-        /*count new lines*/
-        if (c == '\n'){
-            ++nl;
-        }
-        /* checks if you are outside word*/
-        if (c == ' ' || c == '\n' || c == '\t') {
-            state = OUT;
-        }
-            /* counts new words */
-        else if (state == OUT) {
-            state = IN;
-            ++nw;
-        }
+        putchar(c);
+        if (c == ' ' || c == '\n' || c == '\t')
+            putchar('\n');
     }
-
-    printf("New line: %d\n", nl);
-    printf("New word: %d\n", nw);
-    printf("New char: %d\n", nc);
 
 }
